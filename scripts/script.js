58916be9768e057a295478e470023b65ec613e65
Joud Alosaimi <joudalosaimi11@gmail,com>
2025-09-14 01:10:17 +0300

    More script, changed size from px to vh,vw to make it responsive

--- scripts/script.js
+++ scripts/script.js
@@ -6,9 +6,12 @@ function init() {
     let senderSubject = document.querySelector("#subject")
     let senderMessage = document.querySelector("#Message")
     let formButton = document.querySelector("#email-button")
+    let backToTop = document.querySelector("#backToTopBtn")
     // const as learned
     const serviceId = "service_ly1vvf2"
     const templateID = "template_e54tqsj"
+
+
     //function to send the email
     function sendMail(event) {
         event.preventDefault() 
@@ -27,6 +30,25 @@ function init() {
             alert('opps .. something went wrong, please try again ^_^')
         )
     } 
+
+    // copied from W3schools a few edits of my own
+    window.onscroll = function(){scroll()}
+    backToTop.style.display = "none"
+    // console.log(window)
+    function scroll() {
+        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+            backToTop.style.display = "block"
+        } else {
+            backToTop.style.display = "none"
+        }
+    }
+
+    function toTop() {
+        document.body.scrollTop = 0
+        document.documentElement.scrollTop = 0
+    }
+
+    backToTop.addEventListener('click', toTop)
 
     // adding an event listener ass learned
     formButton.addEventListener('click' , sendMail)
